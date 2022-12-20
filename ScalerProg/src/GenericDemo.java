e46a9e61884c137c030f0459a6e0a91bfc4711f0
Pratima1234W <111169518+Pratima1234W@users.noreply.github.com>
2022-12-20 18:44:39 +0530

    hc-scaler

---
+++ ScalerProg/src/GenericDemo.java
@@ -0,0 +1,5 @@
+//Note:By using generic we provide type safety to object.
+//object class is the top most class of the all java class.
+public class GenericDemo {
+
+}
---
+++ ScalerProg/src/assignment/Htreemap.java
@@ -0,0 +1,10 @@
+package assignment;
+
+import java.util.TreeMap;
+
+public class Htreemap {
+	public static void main(String args[]) {
+		TreeMap mymap=new TreeMap<>()
+	}
+
+}
